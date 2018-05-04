6fad2231f7154f3e41e6684f5d68db0857aa1b2d
Layton <layton@rose-hulman.edu>
2018-05-03 20:03:23 -0400

    adding degree dataset

---
+++ R/vignette_data.R
@@ -0,0 +1,19 @@
+#' Vignette data examples
+#'
+#' Several tidy data sets used in the vignettes.
+#'
+#' \describe{
+#'
+#' \item{\code{ever}}{A data frame of students ever in a major.}
+#'
+#' }
+#'
+#' @name vignette_data
+#' @format A data frame
+#' @source MIDFIELD
+#' @examples
+#' ever
+NULL
+
+#' @rdname vignette_data
+"ever"
