b91456419ec9f4f196e4cde11432d5a73751963f
graphdr <graphdoctor@gmail.com>
2020-06-11 21:50:26 -0400

    cip_filter

--- tests/testthat/test-cip_filter.R
+++ tests/testthat/test-cip_filter.R
@@ -2,41 +2,34 @@
 context("test-cip_filter")
 
 # create test case
-case_engr <- cip_filter(series = cip_engr)
+case_engr <- cip_filter(cip, series = cip_engr)
 
-test_that(".data argument NULL", {
-  expect_equal(cip_filter(reference = NULL), cip_filter(reference = cip))
-  expect_equal(cip_filter(series = cip_engr), cip_filter(series = "^14"))
-})
-
-test_that("NULL arguments returns full CIP", {
-  expect_equal(cip, cip_filter(series = NULL, reference = NULL))
-})
-
-test_that("NULL series returns unaltered data", {
-  expect_equal(
-    cip_filter(series = cip_engr),
-    cip_filter(series = NULL, reference = case_engr)
-  )
-})
-
-test_that("regular data frame is converted to tibble", {
-  expect_equal(
-    cip_filter(reference = as.data.frame(case_engr)),
-    cip_filter(reference = case_engr)
-  )
-})
+# test_that(".data argument NULL", {
+#   expect_equal(cip_filter(reference = NULL), cip_filter(reference = cip))
+#   expect_equal(cip_filter(series = cip_engr), cip_filter(series = "^14"))
+# })
+#
+# test_that("NULL arguments returns full CIP", {
+#   expect_equal(cip, cip_filter(series = NULL, reference = NULL))
+# })
+#
+# test_that("NULL series returns unaltered data", {
+#   expect_equal(
+#     cip_filter(series = cip_engr),
+#     cip_filter(series = NULL, reference = case_engr)
+#   )
+# })
 
 test_that("Numeric terms are coverted to strings", {
   expect_equal(
-    cip_filter(series = 140801),
-    cip_filter(series = "140801")
+    cip_filter(cip, series = 140801),
+    cip_filter(cip, series = "140801")
   )
 })
 
 test_that("Invalid series are quietly ignored", {
   expect_equal(
-    cip_filter(series = seq(140800, 140899)),
-    cip_filter(series = "^1408")
+    cip_filter(cip, series = seq(140800, 140899)),
+    cip_filter(cip, series = "^1408")
   )
 })
