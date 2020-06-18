43f3c75f511733872cd6e06f9cf610d2bb293e86
Richard Layton <graphdoctor@gmail.com>
2020-06-18 12:25:45 -0400

    script link

---
+++ inst/script_stickiness.R
@@ -0,0 +1,50 @@
+# packages used
+library("midfieldr")
+library("midfielddata")
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("seplyr")
+
+# CIP information saved from an earlier vignette
+program_group <- case_program_group
+
+# variable names for grouping, summarizing, and joining
+grouping_variables <- c("program", "race", "sex")
+
+# extract the 6-digit CIPs as a search series
+program_series <- program_group[["cip6"]] %>%
+        unique() %>%
+        sort()
+
+# count students ever enrolled in programs
+ever_enrolled <- ever_filter(midfieldterms, codes = program_series) %>%
+	race_sex_join() %>%
+	left_join(., program_group, by = "cip6") %>%
+	group_summarize(., grouping_variables, "ever" = n())
+
+# count students graduating from programs
+graduated <- grad_filter(midfielddegrees, codes = program_series) %>%
+	race_sex_join() %>%
+	left_join(., program_group, by = "cip6") %>%
+	group_summarize(., grouping_variables, "grad" = n())
+
+# tally stickiness, by group
+stickiness <- left_join(ever_enrolled, graduated, by = grouping_variables) %>%
+	filter(ever > 5) %>%
+	mutate(stick = round(grad / ever, 3))
+
+# prepare data for graph
+stickiness_mw <- stickiness %>%
+	filter(!race %in% c("Unknown", "International", "Other")) %>%
+	filter(!sex %in% "Unknown") %>%
+	mutate(race_sex = str_c(race, sex, sep = " ")) %>%
+	select(program, race_sex, stick) %>%
+	multiway_order()
+
+# graph
+ggplot(stickiness_mw, aes(x = stick, y = race_sex)) +
+	facet_wrap(vars(program), ncol = 1, as.table = FALSE) +
+	geom_point(na.rm = TRUE) +
+	labs(x = "Stickiness", y = "") +
+	theme_midfield()
