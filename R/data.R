7035fa022c405b13cab2c9ef73e7e88b104ba892
graphdr <graphdoctor@gmail.com>
2021-07-17 19:53:27 -0400

    docs: edit multiway prose

--- R/data.R
+++ R/data.R
@@ -8,8 +8,8 @@
 #' names for 1582 instructional programs organized on three levels: a
 #' 2-digit series, a 4-digit series, and a 6-digit series.
 #'
-#' The midfielddata taxonomy includes one non-IPEDS code (999999) for 
-#' Undecided or Unspecified, instances in which institutions reported no 
+#' The midfielddata taxonomy includes one non-IPEDS code (999999) for
+#' Undecided or Unspecified, instances in which institutions reported no
 #' program information or that students were not enrolled in a program.
 #'
 #' @format A \code{data.table} with 1582 rows and 6 columns keyed by the
