81683a247ab93d63d42e1dec09e35234fda646b5
graphdr <graphdoctor@gmail.com>
2022-06-29 09:12:27 -0400

    cleanup

--- man/examples/add_completion_status_exa.R
+++ man/examples/add_completion_status_exa.R
@@ -2,11 +2,11 @@
 dframe <- toy_student[1:10, .(mcid)]
 
 # Timely term column is required to add completion status column
-dframe <- add_timely_term(dframe, midfield_term = toy_term)
+dframe <- add_timely_term(dframe, toy_term)
 
 # Add completion status column
-add_completion_status(dframe, midfield_degree = toy_degree)
+add_completion_status(dframe, toy_degree)
 
 # Existing completion_status column, if any, is overwritten
 dframe[, completion_status := NA_character_]
-add_completion_status(dframe, midfield_degree = toy_degree)
+add_completion_status(dframe, toy_degree)
